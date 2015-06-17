34d26b996519cff287ce397fa8ee773bdb90c10a
Eric Baer <me@ericbaer.com>
2015-06-16 21:14:49 -0700

    Add best practices

--- defaults.js
+++ defaults.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var bestPractices = require('./best-practices');
 var errors = require('./errors');
 var es6 = require('./es6');
 var legacy = require('./legacy');
@@ -60,136 +61,7 @@ var defaults = {
 
     env: {
         node: true
-    },
-
-    rules: {
-        // # Best Practices
-        // These are rules designed to prevent you from making mistakes. They either
-        // prescribe a better way of doing something or help you avoid footguns.
-
-        // Enforces getter/setter pairs in objects
-        'accessor-pairs': 2,
-        // treat var statements as if they were block scoped
-        'block-scoped-var': 0,
-        // specify the maximum cyclomatic complexity allowed in a program
-        'complexity': [
-            2,
-        // Maximum cycolmatic complexity
-            10,
-        ],
-      // require return statements to either always or never specify values
-        'consistent-return': 2,
-        // specify curly brace conventions for all control statements
-        'curly': 2,
-        // require default case in switch statements
-        'default-case': 2,
-        // encourages use of dot notation whenever possible
-        'dot-notation': 2,
-        // enforces consistent newlines before or after dots
-        'dot-location': [
-            2,
-            // require the dot to be on the same line as the property
-            'property',
-        ],
-        // require the use of === and !==
-        'eqeqeq': 2,
-        // make sure for-in loops have an if statement
-        'guard-for-in': 0,
-        // disallow the use of alert, confirm, and prompt
-        'no-alert': 2,
-        // disallow use of arguments.caller or arguments.callee
-        'no-caller': 2,
-        // disallow division operators explicitly at beginning of regular expression
-        'no-div-regex': 2,
-        // disallow else after a return in an if
-        'no-else-return': 2,
-        // disallow use of labels for anything other then loops and switches
-        'no-empty-label': 2,
-        // disallow comparisons to null without a type-checking operator
-        'no-eq-null': 2,
-        // disallow use of eval()
-        'no-eval': 2,
-        // disallow adding to native types
-        'no-extend-native': 2,
-        // disallow unnecessary function binding
-        'no-extra-bind': 2,
-        // disallow fallthrough of case statements
-        'no-fallthrough': 2,
-        // disallow the use of leading or trailing decimal points in numeric literals
-        'no-floating-decimal': 2,
-        // disallow use of eval()-like methods
-        'no-implied-eval': 2,
-        // disallow usage of __iterator__ property
-        'no-iterator': 2,
-        // disallow use of labeled statements
-        'no-labels': 0,
-        // disallow unnecessary nested blocks
-        'no-lone-blocks': 2,
-        // disallow creation of functions within loops
-        'no-loop-func': 2,
-        // disallow use of multiple spaces
-        'no-multi-spaces': 2,
-        // disallow use of multiline strings
-        'no-multi-str': 2,
-        // disallow reassignments of native objects
-        'no-native-reassign': 2,
-        // disallow use of new operator when not part of the assignment or comparison
-        'no-new': 2,
-        // disallow use of new operator for Function object
-        'no-new-func': 2,
-        // disallows creating new instances of String,Number, and Boolean
-        'no-new-wrappers': 2,
-        // disallow use of (old style) octal literals
-        'no-octal': 2,
-        // disallow use of octal escape sequences in string literals, such as var foo = "Copyright \251";
-        'no-octal-escape': 2,
-        // disallow reassignment of function parameters
-        'no-param-reassign': 2,
-        // disallow use of process.env
-        'no-process-env': 0,
-        // disallow usage of __proto__ property
-        'no-proto': 2,
-        // disallow declaring the same variable more then once
-        'no-redeclare': 2,
-        // disallow use of assignment in return statement
-        'no-return-assign': 2,
-        // disallow use of `javascript:` urls.
-        'no-script-url': 2,
-        // disallow comparisons where both sides are exactly the same
-        'no-self-compare': 2,
-        // disallow use of comma operator
-        'no-sequences': 2,
-        // restrict what can be thrown as an exception
-        'no-throw-literal': 2,
-        // disallow usage of expressions in statement position
-        'no-unused-expressions': 2,
-        // disallow use of void operator
-        'no-void': 2,
-        // disallow usage of configurable warning terms in comments: e.g. todo
-        'no-warning-comments': [
-            2,
-            {
-                // Disallowed warning terms
-                'terms': [
-                    'todo',
-                    'fixme',
-                    'xxx',
-                ],
-                // Comment location to check
-                'location': 'anywhere',
-            },
-        ],
-        // disallow use of the with statement
-        'no-with': 2,
-        // require use of the second argument for parseInt()
-        'radix': 2,
-        // requires to declare all vars on top of their containing scope
-        'vars-on-top': 0,
-        // require immediate function invocation to be wrapped in parentheses
-        'wrap-iife': 2,
-        // require or disallow Yoda conditions
-        'yoda': 2
-    },
+    }
 };
 
-module.exports = _.merge(defaults, errors, es6, legacy, node, strict, style, variables);
+module.exports = _.merge(defaults, bestPractices, errors, es6, legacy, node, strict, style, variables);
