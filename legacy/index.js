5876aa4f5aafbcb58c08d56804bd5e563d5f6ba7
Eric Baer <me@ericbaer.com>
2015-06-17 00:36:06 -0700

    Correct small programming error

--- legacy/index.js
+++ legacy/index.js
@@ -6,16 +6,16 @@ var legacy = _.cloneDeep(require('./off.js'));
 var legacyRules = legacy.rules;
 
 // specify the maximum depth that blocks can be nested
-legacyRules.['max-depth'] = 0;
+legacyRules['max-depth'] = 0;
 // specify the maximum length of a line in your program
-legacyRules.['max-len'] = [2, 100, 4];
+legacyRules['max-len'] = [2, 100, 4];
 // limits the number of parameters that can be used in the function declaration.
-legacyRules.['max-params'] = [1, 5];
+legacyRules['max-params'] = [1, 5];
 // specify the maximum number of statement allowed in a function
-legacyRules.['max-statements'] = 0;
+legacyRules['max-statements'] = 0;
 // disallow use of bitwise operators
-legacyRules.['no-bitwise'] = 0;
+legacyRules['no-bitwise'] = 0;
 // disallow use of unary operators, ++ and --
-legacyRules.['no-plusplus'] = 0;
+legacyRules['no-plusplus'] = 0;
 
 module.exports = legacy;
