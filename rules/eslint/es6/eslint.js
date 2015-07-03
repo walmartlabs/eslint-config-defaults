7135f9c129c30f4732595d0b56be4e33c75a17a1
Eric Baer <me@ericbaer.com>
2015-07-03 12:20:41 -0700

    Add eslint defaults to rules

---
+++ rules/eslint/es6/eslint.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  env: {
+    es6: false
+  },
+  ecmaFeatures: {
+    modules: false
+  },
+  rules: {
+    // enforce the spacing around the * in generator functions
+    'generator-star-spacing': 0,
+    // require let or const instead of var
+    'no-var': 0,
+    // require method and property shorthand syntax for object literals
+    'object-shorthand': 0,
+    // suggest using of const declaration for variables that are never modified after declared
+    'prefer-const': 0
+  }
+};
