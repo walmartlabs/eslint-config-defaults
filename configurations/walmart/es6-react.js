bcd2391825198170bf75197b766cdb1bbbadd6c1
Eric Baer <me@ericbaer.com>
2015-07-05 17:16:45 -0700

    Add React configuration

---
+++ configurations/walmart/es6-react.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _ = require("lodash");
+
+module.exports = _.merge({},
+  require("./es6.js"),
+  require("../../rules/react/walmart.js"), {
+    "globals": {
+      "fetch": false              // Expect a `fetch` polyfill.
+    },
+    "rules": {
+      "no-extra-parens": 0        // Want `return (<Foo />);` to be allowed.
+      "no-var": 2
+    }
+  }
+);
--- rules/react/walmart.js
+++ rules/react/walmart.js
@@ -1,6 +1,7 @@
 "use strict";
 
 module.exports = {
+  "parser": "babel-eslint"
   "plugins": [
     "react"
   ],
@@ -9,40 +10,40 @@ module.exports = {
   }
   "rules": {
     // Prevent missing displayName in a React component definition
-    "display-name": 0,
+    "react/display-name": 0,
     // Enforce boolean attributes notation in JSX
-    "jsx-boolean-value": 0,
+    "react/jsx-boolean-value": 2,
     // Disallow undeclared variables in JSX
-    "jsx-no-undef": 0,
+    "react/jsx-no-undef": 2,
     // Enforce quote style for JSX attributes
-    "jsx-quotes": 0,
+    "react/jsx-quotes": 2,
     // Enforce propTypes declarations alphabetical sorting
-    "jsx-sort-prop-types": 0,
+    "react/jsx-sort-prop-types": 0,
     // Enforce props alphabetical sorting
-    "jsx-sort-props": 0,
+    "react/jsx-sort-props": 0,
     // Prevent React to be incorrectly marked as unused
-    "jsx-uses-react": 0,
+    "react/jsx-uses-react": 2,
     // Prevent variables used in JSX to be incorrectly marked as unused
-    "jsx-uses-vars": 0,
+    "react/jsx-uses-vars": 2,
     // Prevent usage of setState in componentDidMount
-    "no-did-mount-set-state": 0,
+    "react/no-did-mount-set-state": 2,
     // Prevent usage of setState in componentDidUpdate
-    "no-did-update-set-state": 0,
+    "react/no-did-update-set-state": 2,
     // Prevent multiple component definition per file
-    "no-multi-comp": 0,
+    "react/no-multi-comp": 2,
     // Prevent usage of unknown DOM property
-    "no-unknown-property": 0,
+    "react/no-unknown-property": 2,
     // Prevent missing props validation in a React component definition
-    "prop-types": 0,
+    "react/prop-types": 2,
     // Prevent missing React when using JSX
-    "react-in-jsx-scope": 0,
+    "react/react-in-jsx-scope": 2,
     // Restrict file extensions that may be required
-    "require-extension": 0,
+    "react/require-extension": 0,
     // Prevent extra closing tags for components without children
-    "self-closing-comp": 0,
+    "react/self-closing-comp": 2,
     // Enforce component methods order
-    "sort-comp": 0,
+    "react/sort-comp": 0,
     // Prevent missing parentheses around multilines JSX
-    "wrap-multilines": 0
+    "react/wrap-multilines": 2
   }
 }
