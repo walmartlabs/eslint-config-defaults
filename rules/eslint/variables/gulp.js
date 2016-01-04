9243f55d7b457bc9d87c3176c48aa99439484a84
Eric Baer <me@ericbaer.com>
2016-01-04 13:10:53 -0800

    Add gulp ginfig

---
+++ rules/eslint/variables/gulp.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  "rules": {
+    // enforce or disallow variable initializations at definition
+    "init-declarations": 0,
+    // disallow the catch clause parameter name being the same as a variable in the outer scope
+    "no-catch-shadow": 0,
+    // disallow deletion of variables
+    "no-delete-var": 0,
+    // disallow labels that share a name with a variable
+    "no-label-var": 0,
+    // disallow shadowing of names such as arguments
+    "no-shadow-restricted-names": 0,
+    // disallow declaration of variables already declared in the outer scope
+    "no-shadow": 0,
+    // disallow use of undefined when initializing variables
+    "no-undef-init": 0,
+    // disallow use of undeclared variables unless mentioned in a /*global */ block
+    "no-undef": 0,
+    // disallow use of undefined variable
+    "no-undefined": 0,
+    // disallow declaration of variables that are not used in the code
+    "no-unused-vars": 1,
+    // disallow use of variables before they are defined
+    "no-use-before-define": [2, "nofunc"]
+  }
+};
