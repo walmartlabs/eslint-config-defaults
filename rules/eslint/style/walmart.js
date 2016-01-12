18508711c94ac0621fdd7883d871007de572abb4
Jan Nordén <jan.norden@gmail.com>
2016-01-12 15:47:04 +0100

    Update walmart.js
    Remove duplicate space-before-keywords entry

--- rules/eslint/style/walmart.js
+++ rules/eslint/style/walmart.js
@@ -112,14 +112,12 @@ module.exports = {
     "sort-vars": 0,
     // require a space after certain keywords
     "space-after-keywords": [2, "always"],
-    // require a space before certain keywords
-    "space-before-keywords": 0,
+    // require a space before certain keywords (fixable)
+    "space-before-keywords": [2, "always"],
     // require or disallow space before blocks
     "space-before-blocks": [2, "always"],
     // require or disallow space before function opening parenthesis
     "space-before-function-paren": [2, {"anonymous": "always", "named": "never" }],
-    // require a space before certain keywords (fixable)
-    "space-before-keywords": [2, "always"],
     // require or disallow spaces inside parentheses
     "space-in-parens": [2, "never"],
     // require spaces around operators
